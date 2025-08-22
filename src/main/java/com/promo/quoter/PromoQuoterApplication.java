88c97d12a70359b305fef9133b69300a21b4b292
Paul <paul@example.com>
2025-08-22 09:40:35 +0300

    Initial commit

---
+++ src/main/java/com/promo/quoter/PromoQuoterApplication.java
@@ -0,0 +1,13 @@
+package com.promo.quoter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoQuoterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoQuoterApplication.class, args);
+	}
+
+}
